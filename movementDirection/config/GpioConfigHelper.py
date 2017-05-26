8854ac8cd3c5730b00a10dfcbce9b5c840e42d1a
kochmichael <michael.mammut@gmail.com>
2017-05-26 21:55:49 +0200

    Change False to 0 at GPIO.ouput()

--- movementDirection/config/GpioConfigHelper.py
+++ movementDirection/config/GpioConfigHelper.py
@@ -6,12 +6,12 @@ from .GpioPinConfig import GpioPinConfig as pin
 class GpioConfiHelper:
     # Set all Pins to low
     def all_off(self):
-        GPIO.output(pin.ENABLE_ENGINE_A, False)
-        GPIO.output(pin.ENABLE_ENGINE_B, False)
-        GPIO.output(pin.INPUT1, False)
-        GPIO.output(pin.INPUT2, False)
-        GPIO.output(pin.INPUT3, False)
-        GPIO.output(pin.INPUT4, False)
+        GPIO.output(pin.ENABLE_ENGINE_A, 0)
+        GPIO.output(pin.ENABLE_ENGINE_B, 0)
+        GPIO.output(pin.INPUT1, 0)
+        GPIO.output(pin.INPUT2, 0)
+        GPIO.output(pin.INPUT3, 0)
+        GPIO.output(pin.INPUT4, 0)
         return "ALL GPIOS OFF"
 
     # Define all Pins to output
--- movementDirection/movements/Forward.py
+++ movementDirection/movements/Forward.py
@@ -5,11 +5,11 @@ from ..config.GpioPinConfig import GpioPinConfig as pin
 class Forward(DirectionAbstract):
 
     def direction(self):
-       GPIO.output(pin.ENABLE_ENGINE_A, True)
-       GPIO.output(pin.ENABLE_ENGINE_B, True)
-       GPIO.output(pin.INPUT1, True)
-       GPIO.output(pin.INPUT2, False)
-       GPIO.output(pin.INPUT3, True)
-       GPIO.output(pin.INPUT4, False)
+       GPIO.output(pin.ENABLE_ENGINE_A, 1)
+       GPIO.output(pin.ENABLE_ENGINE_B, 1)
+       GPIO.output(pin.INPUT1, 1)
+       GPIO.output(pin.INPUT2, 0)
+       GPIO.output(pin.INPUT3, 1)
+       GPIO.output(pin.INPUT4, 0)
        return "FORWARD"
 
