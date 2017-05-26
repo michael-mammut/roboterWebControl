228cf8f56fe5d9f7aeb153bbafb0f28908401540
kochmichael <michael.mammut@gmail.com>
2017-05-26 22:29:00 +0200

    get Value from enum

--- movementDirection/config/GpioConfigHelper.py
+++ movementDirection/config/GpioConfigHelper.py
@@ -6,12 +6,12 @@ from .GpioPinConfig import GpioPinConfig as pin
 class GpioConfiHelper:
     # Set all Pins to low
     def all_off(self):
-        GPIO.output(pin.ENABLE_ENGINE_A.value, str(0))
-        GPIO.output(pin.ENABLE_ENGINE_B.value, str(0))
-        GPIO.output(pin.INPUT1.value, str(0))
-        GPIO.output(pin.INPUT2.value, str(0))
-        GPIO.output(pin.INPUT3.value, str(0))
-        GPIO.output(pin.INPUT4.value, str(0))
+        GPIO.output(pin.ENABLE_ENGINE_A.value, 0)
+        GPIO.output(pin.ENABLE_ENGINE_B.value, 0)
+        GPIO.output(pin.INPUT1.value, 0)
+        GPIO.output(pin.INPUT2.value, 0)
+        GPIO.output(pin.INPUT3.value, 0)
+        GPIO.output(pin.INPUT4.value, 0)
         return "ALL GPIOS OFF"
 
     # Define all Pins to output
--- movementDirection/movements/Forward.py
+++ movementDirection/movements/Forward.py
@@ -5,11 +5,11 @@ from ..config.GpioPinConfig import GpioPinConfig as pin
 class Forward(DirectionAbstract):
 
     def direction(self):
-       GPIO.output(pin.ENABLE_ENGINE_A.value, str(1))
-       GPIO.output(pin.ENABLE_ENGINE_B.value, str(1))
-       GPIO.output(pin.INPUT1.value, str(1))
-       GPIO.output(pin.INPUT2.value, str(0))
-       GPIO.output(pin.INPUT3.value, str(1))
-       GPIO.output(pin.INPUT4.value, str(0))
+       GPIO.output(pin.ENABLE_ENGINE_A.value, 1)
+       GPIO.output(pin.ENABLE_ENGINE_B.value, 1)
+       GPIO.output(pin.INPUT1.value, 1)
+       GPIO.output(pin.INPUT2.value, 0)
+       GPIO.output(pin.INPUT3.value, 1)
+       GPIO.output(pin.INPUT4.value, 0)
        return "FORWARD"
 
