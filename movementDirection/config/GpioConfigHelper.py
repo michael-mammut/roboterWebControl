d6e28fb2e6973755214d47865cb33a87f80289e5
kochmichael <michael.mammut@gmail.com>
2017-05-26 22:25:32 +0200

    get Value from enum

--- movementDirection/config/GpioConfigHelper.py
+++ movementDirection/config/GpioConfigHelper.py
@@ -6,20 +6,20 @@ from .GpioPinConfig import GpioPinConfig as pin
 class GpioConfiHelper:
     # Set all Pins to low
     def all_off(self):
-        GPIO.output(pin.ENABLE_ENGINE_A, str(0))
-        GPIO.output(pin.ENABLE_ENGINE_B, str(0))
-        GPIO.output(pin.INPUT1, str(0))
-        GPIO.output(pin.INPUT2, str(0))
-        GPIO.output(pin.INPUT3, str(0))
-        GPIO.output(pin.INPUT4, str(0))
+        GPIO.output(pin.ENABLE_ENGINE_A.value, str(0))
+        GPIO.output(pin.ENABLE_ENGINE_B.value, str(0))
+        GPIO.output(pin.INPUT1.value, str(0))
+        GPIO.output(pin.INPUT2.value, str(0))
+        GPIO.output(pin.INPUT3.value, str(0))
+        GPIO.output(pin.INPUT4.value, str(0))
         return "ALL GPIOS OFF"
 
     # Define all Pins to output
     def all_out(self):
-        GPIO.setup(pin.ENABLE_ENGINE_A, GPIO.OUT)
-        GPIO.setup(pin.ENABLE_ENGINE_B, GPIO.OUT)
-        GPIO.setup(pin.INPUT1, GPIO.OUT)
-        GPIO.setup(pin.INPUT2, GPIO.OUT)
-        GPIO.setup(pin.INPUT3, GPIO.OUT)
-        GPIO.setup(pin.INPUT4, GPIO.OUT)
+        GPIO.setup(pin.ENABLE_ENGINE_A.value, GPIO.OUT)
+        GPIO.setup(pin.ENABLE_ENGINE_B.value, GPIO.OUT)
+        GPIO.setup(pin.INPUT1.value, GPIO.OUT)
+        GPIO.setup(pin.INPUT2.value, GPIO.OUT)
+        GPIO.setup(pin.INPUT3.value, GPIO.OUT)
+        GPIO.setup(pin.INPUT4.value, GPIO.OUT)
         return "ALL GPIOS OUTPUT"
--- movementDirection/movements/Forward.py
+++ movementDirection/movements/Forward.py
@@ -5,11 +5,11 @@ from ..config.GpioPinConfig import GpioPinConfig as pin
 class Forward(DirectionAbstract):
 
     def direction(self):
-       GPIO.output(pin.ENABLE_ENGINE_A, str(1))
-       GPIO.output(pin.ENABLE_ENGINE_B, str(1))
-       GPIO.output(pin.INPUT1, str(1))
-       GPIO.output(pin.INPUT2, str(0))
-       GPIO.output(pin.INPUT3, str(1))
-       GPIO.output(pin.INPUT4, str(0))
+       GPIO.output(pin.ENABLE_ENGINE_A.value, str(1))
+       GPIO.output(pin.ENABLE_ENGINE_B.value, str(1))
+       GPIO.output(pin.INPUT1.value, str(1))
+       GPIO.output(pin.INPUT2.value, str(0))
+       GPIO.output(pin.INPUT3.value, str(1))
+       GPIO.output(pin.INPUT4.value, str(0))
        return "FORWARD"
 
