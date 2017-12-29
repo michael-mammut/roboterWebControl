b31d9d5c0322a04a6426e0944e99991abac031c0
kochmichael <michael.mammut@gmail.com>
2017-12-29 11:29:24 +0100

    DummyTests hinzugefügt

--- movementDirection/tests.py
+++ movementDirection/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase as djangoTestCase
+import os
+import unittest
 
 # Create your tests here.
+class TestExample(djangoTestCase):
+
+    def setUp(self):
+        print('setup')
+        print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    def test_abc(self):
+        self.assertEqual(1,1)
+
+    def test_def(self):
+        self.assertEqual(1,1)
+
+    def tearDown(self):
+        print('tearDown')
